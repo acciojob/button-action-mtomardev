97f54bde9ac12e69f9a6c491e916e7f14ca17980
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-06 19:43:56 +0530

    button

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import './../styles/App.css';
 import 'style.css';
 
 const App = (props) => {
+  let[visible , setVisible] = useState("false")
+
+  const toggleVisible = () => {
+    setVisible(true)
+  }
 
   return (
     <div className="App" id="main">
-      <button id="click">Click</button>
-      <p id="para" className="hide show" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={toggleVisible}>Click</button>
+      <p id="para" className = {visible ? "show" : "hide" } >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </div>
   );
 }
